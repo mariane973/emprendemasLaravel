fcfc7e01a38b01ce3fd8c63af114fc6acb29e2fc
mariane973 <mariane9quintero@gmail.com>
2024-07-19 04:12:53 -0500

    Cambio nabvar LS

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -12,179 +12,150 @@
     <link rel="shortcut icon" type="image/png" href="/imagenes/icon-tucan.png"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <!-- <style>
-        .tailwind {
-            @import url('https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css');
-        }
-    </style> -->
     @livewireStyles
+    <style>
+        /* Estilo para la imagen del logo */
+        .navbar-brand img {
+            height: 52px;
+        }
+
+        .navbar-nav {
+            margin-left: auto;
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+        }
+
+    
+    </style>
 </head>
 <body class="@yield('body-class', 'bg')">
-    <nav>   
-        <div class="Navbar container-fluid fixed-top" style="background-color: white; ">    
-            <div class="row">
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" style="background-color: white;">
+        <div class="container-fluid d-flex">
+    
+            <!-- Imagen del logo (visible en todas las pantallas) -->
             @cannot('verCarrito')
-                <div class="Logo col-lg-2 ms-lg-5 col-md-2 col-sm-1  col-xs-1 offset-sm-12 d-flex  me-sm-2 mb-sm-4  col-xs-1 me-xs-2">
-                    <a href="/" class="fw-bold">
-                        <img src="/imagenes/op2.png" alt="" style="height: 52px; ">
-                    </a>
-                </div>
-                @endcannot
-
-
-                
-                @can('verCarrito')
-                <div class="InicioSesion align-items-end justify-content-end  d-flex col-lg-4 mt-2 col-sm-6 col-md-4  mb-4 ">
-                <a href="/" class="fw-bold">
-                        <img src="/imagenes/op2.png" alt="" style="height: 52px; " class="">
-                    </a>
-                    <div class="Usuario col ms-1 mb-2 vertical-align: middle;">
-                    <livewire:cartcontador />
-                    </div>
-                </div>
-
-                <div class="InicioSesion col-lg-2 col-md-2 col-sm-3 col-xs-1 offset-md-3 offset-lg-4 align-items-end justify-content-end d-flex mb-sm-4">
-                <div class="dropdown" id="userDropdown">
-                    <i class="fas fa-seedling icono" style="font-size: 25px; vertical-align: middle;"></i>
-                    <a href="/servicios" class="fw-bold">Conocer</a>
-                    <div class="dropdown-content">
-                        <a href="/servicios">Servicios</a>
-                        <a href="/productos">Productos</a>
-                    </div>
-                </div>
-            </div>
-                
-                @endcan
-                            
-
-            @cannot('verCarrito')
-            <div class="InicioSesion col-lg-2 col-md-3 col-sm-3 col-xs-1   @cannot('verCarrito') offset-lg-5 offset-sm-4 offset-xs-1 @endcannot align-items-end justify-content-end d-flex mb-sm-4">
-                <div class="dropdown" id="userDropdown">
-                    <i class="fas fa-seedling icono" style="font-size: 25px; vertical-align: middle;"></i>
-                    <a href="/servicios" class="fw-bold">Conocer</a>
-                    <div class="dropdown-content">
-                        <a href="/servicios">Servicios</a>
-                        <a href="/productos">Productos</a>
-                    </div>
-                </div>
-            </div>
-            
+            <a class="navbar-brand d-md-none d-lg-block" href="/">
+                <img src="/imagenes/op2.png" alt="Logo">
+            </a>
             @endcannot
-                
-                <div class="InicioSesion col-lg-2 col-md-3 col-sm-3   col-xs-6  justify-content-start d-flex mb-sm-4 col-md-1">
-                    @guest
-                        <div class="Usuario " style="display: flex; align-items: center;">
-                        <i class="fas fa-user-circle iconos" style="font-size: 30px; vertical-align: middle;"></i>
-                            <a href="{{ route('login') }}"  class="fw-bold ms-2">Ingresar</a>
+
+            @can('verCarrito')
+            <a class="navbar-brand d-md-none d-lg-block" href="/">
+                <img src="/imagenes/op2.png" alt="Logo">
+            </a>
+            <div class="Usuario ms-xs-5 ps-xs-5  ms-1 mt-3 mb-2">
+                <livewire:cartcontador />
+            </div>
+            @endcan
+
+            <!-- Botón de hamburguesa para pantallas pequeñas -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <!-- Contenido del menú -->
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto">
+                    @can('verCarrito')
+                    <li class="nav-item d-md-none d-lg-flex align-items-end">
+                        <div class="dropdown">
+                            <button class="btn btn-link dropdown-toggle mt-2 me-2" type="button" id="userDropdown" data-bs-toggle="dropdown" aria-expanded="false" style="text-decoration: none !important;">
+                                <i class="fas fa-seedling icono" style="font-size: 25px; vertical-align: middle;"></i>
+                                <a href="/servicios" class="fw-bold"  style="text-decoration: none !important; font-size: 18px; color: #33B66C;">Conocer</a>
+                            </button>
+                            <ul class="dropdown-menu">
+                                <li><a href="/servicios" class="dropdown-item">Servicios</a></li>
+                                <li><a href="/productos" class="dropdown-item">Productos</a></li>
+                            </ul>
                         </div>
-                    @else
+                    </li>
+                    @endcan
+                    @cannot('verCarrito')
+                    <li class="nav-item d-md-none d-lg-flex align-items-end">
+                        <div class="dropdown">
+                            <button class="btn btn-link dropdown-toggle mt-2 me-2" type="button" id="userDropdown" data-bs-toggle="dropdown" aria-expanded="false" style="text-decoration: none !important;">
+                                <i class="fas fa-seedling icono" style="font-size: 25px; vertical-align: middle;"></i>
+                                <a href="/servicios" class="fw-bold"  style="text-decoration: none !important; font-size: 18px; color: #33B66C;">Conocer</a>
+                            </button>
+                            <ul class="dropdown-menu">
+                                <li><a href="/servicios" class="dropdown-item">Servicios</a></li>
+                                <li><a href="/productos" class="dropdown-item">Productos</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    @endcannot
+                    <li class="nav-item d-md-none d-lg-flex">
+                        @guest
+                        <div class="Usuario ms-3 mt-2" style="display: flex; align-items: center;"  style="text-decoration: none !important;">
+                            <i class="fas fa-user-circle iconos" style="font-size: 30px; vertical-align: middle;"></i>
+                            <a href="{{ route('login') }}" class="fw-bold ms-2 me-5 pe-5" style="text-decoration: none !important; font-size: 18px; color: #33B66C;">Ingresar</a>
+                        </div>
+                        @else
+                        <!-- Botón de dropdown para usuario -->
                         <div class="Usuario col align-items-end">
-                            <div class="dropdown" id="userDropdown">
-                            <i class="fas fa-user-circle iconon" style="font-size: 30px; vertical-align: middle;"></i>
-                                <span class="fw-bold" id="userName" style="vertical-align: middle;">{{ Auth::user()->name }}</span>
-                                <div class="dropdown-content">
-                                    <a href="{{ route('profile') }}">Perfil</a>
-                                    <a href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                    document.getElementById('logout-form').submit();">
-                                        Cerrar Sesión
-                                    </a>
-                                </div>
+                            <div class="dropdown">
+                                <button class="btn btn-link dropdown-toggle me-5 pe-5 mt-2" type="button" id="userDropdown" data-bs-toggle="dropdown" aria-expanded="false" style="text-decoration: none !important;">
+                                    <i class="fas fa-user-circle iconon me-1" style="font-size: 30px; vertical-align: middle;"></i>
+                                    <span class="fw-bold" id="userName" style="text-decoration: none !important;">{{ Auth::user()->name }}</span>
+                                </button>
+                                <ul class="dropdown-menu" aria-labelledby="userDropdown">
+                                    <li><a class="dropdown-item" href="{{ route('profile') }}">Perfil</a></li>
+                                    <li>
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                            Cerrar Sesión
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            @csrf
+                                        </form>
+                                    </li>
+                                </ul>
                             </div>
                         </div>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                            @csrf
-                        </form>
-                    @endguest
-                </div>  
+                        @endguest
+                    </li>
+                </ul>
             </div>
         </div>
     </nav>
     <div class="container-fluid">
         <hr>
     </div>
-    <main class="py-4 " style="margin-top: 5rem;">
+    <main class="py-4" style="margin-top: 5rem;">
         @yield('content')
     </main>
 
-<footer class="bg-body-tertiary text-center mt-5 ">
-    <!-- Grid container -->
-    <div class="container p-4 pb-0">
-        <!-- Section: Social media -->
-        <section class="mb-4">
-        <!-- Facebook -->
-        <a
-        data-mdb-ripple-init
-            class="btn text-white btn-floating m-1"
-            style="background-color: #3b5998;"
-            href="#!"
-            role="button"
-            ><i class="fab fa-facebook-f"></i
-        ></a>
-
-        <!-- Twitter -->
-        <a
-            data-mdb-ripple-init
-            class="btn text-white btn-floating m-1"
-            style="background-color: #55acee;"
-            href="#!"
-            role="button"
-            ><i class="fab fa-twitter"></i
-        ></a>
-
-        <!-- Google -->
-        <a
-            data-mdb-ripple-init
-            class="btn text-white btn-floating m-1"
-            style="background-color: #dd4b39;"
-            href="#!"
-            role="button"
-            ><i class="fab fa-google"></i
-        ></a>
-
-        <!-- Instagram -->
-        <a
-            data-mdb-ripple-init
-            class="btn text-white btn-floating m-1"
-            style="background-color: #ac2bac;"
-            href="#!"
-            role="button"
-            ><i class="fab fa-instagram"></i
-        ></a>
-
-        <!-- Linkedin -->
-        <a
-            data-mdb-ripple-init
-            class="btn text-white btn-floating m-1"
-            style="background-color: #0082ca;"
-            href="#!"
-            role="button"
-            ><i class="fab fa-linkedin-in"></i
-        ></a>
-        <!-- Github -->
-        <a
-            data-mdb-ripple-init
-            class="btn text-white btn-floating m-1"
-            style="background-color: #333333;"
-            href="#!"
-            role="button"
-            ><i class="fab fa-github"></i
-        ></a>
-        </section>
-        <!-- Section: Social media -->
-    </div>
-    <!-- Grid container -->
-
-    <!-- Copyright -->
-    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
-        <p class="text-body">&copy; 2024 Company, Inc</p>
-    </div>
-    <!-- Copyright -->
+    <footer class="bg-body-tertiary text-center mt-5">
+        <!-- Grid container -->
+        <div class="container p-4 pb-0">
+            <!-- Section: Social media -->
+            <section class="mb-4">
+                <!-- Facebook -->
+                <a data-mdb-ripple-init class="btn text-white btn-floating m-1" style="background-color: #3b5998;" href="#!" role="button"><i class="fab fa-facebook-f"></i></a>
+                <!-- Twitter -->
+                <a data-mdb-ripple-init class="btn text-white btn-floating m-1" style="background-color: #55acee;" href="#!" role="button"><i class="fab fa-twitter"></i></a>
+                <!-- Instagram -->
+                <a data-mdb-ripple-init class="btn text-white btn-floating m-1" style="background-color: #ac2bac;" href="#!" role="button"><i class="fab fa-instagram"></i></a>
+                <!-- Linkedin -->
+                <a data-mdb-ripple-init class="btn text-white btn-floating m-1" style="background-color: #0082ca;" href="#!" role="button"><i class="fab fa-linkedin-in"></i></a>
+                <!-- Github -->
+                <a data-mdb-ripple-init class="btn text-white btn-floating m-1" style="background-color: #333333;" href="#!" role="button"><i class="fab fa-github"></i></a>
+            </section>
+            <!-- Section: Social media -->
+        </div>
+        <!-- Grid container -->
+        <!-- Copyright -->
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
+            <p class="text-body">&copy; 2024 Company, Inc</p>
+        </div>
+        <!-- Copyright -->
     </footer>
+
     <script src="{{ asset('js/oferta.js') }}"></script>
     <script src="{{ asset('js/app.js') }}"></script>
     @livewireScripts
-
 </body>
 </html>
 
